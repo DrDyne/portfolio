77981c79fc36d96344265bbe76e1e5209e5b156f
remy <remy_chaumard@silanis.com>
2014-01-19 12:58:45 -0500

    simplified jqSliderCustomlink attr for jq-slider directive

--- app/scripts/directives/slider.js
+++ app/scripts/directives/slider.js
@@ -9,7 +9,7 @@ angular.module('portfolioApp')
     element.sudoSlider({
       responsive: true,
       speed: 400,
-      customLink: '.' + attrs.jqSliderCustomlink,
+      customLink: '.slider-nav-' + attrs.jqSliderCustomlink + ' .jq-slider-link',
       controlsFade: true,
       prevNext: false,
     });
