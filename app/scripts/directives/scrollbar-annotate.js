5773e230ff651af87a887372f0a31ad119c8c583
remy <remy_chaumard@silanis.com>
2014-01-21 21:09:49 -0500

    set logo container to 50px height / width

--- app/scripts/directives/scrollbar-annotate.js
+++ app/scripts/directives/scrollbar-annotate.js
@@ -2,6 +2,7 @@ angular.module('portfolioApp')
 .directive('scrollbarAnnotate', function () {
   return function (scope, element, attrs) {
     $(window).sausage({
+      classes: 'hidden-xs',
       content: function (i, $page) {
         return '<span class="sausage-span">' + $page.attr('id') + '</span>'
       },
