9b4bf99df308b19d2069c69f95003ad5109eb4c9
remy <remy_chaumard@silanis.com>
2014-01-24 12:17:59 -0500

    added link fading effect on hover, reset colors for angular demo

--- app/scripts/controllers/demo-angular.js
+++ app/scripts/controllers/demo-angular.js
@@ -4,12 +4,11 @@ angular.module('portfolioApp')
 .controller('DemoAngularCtrl', [
   '$scope',
 function ($scope) {
-  var randomColor = function () { return '#' + ('00000' + (Math.random() * 16777216 << 0).toString(16)).substr(-6); }
+  $scope.initColors = function () { return ['#ffe600', 'rgba(255,230,0,0.5)', 'transparent', 'rgba(255,127,127,0.5)', 'red'] }
 
   $scope.updateGradient = function (colors) {
     var gradient = [];
     angular.forEach($scope.colors, function (value, index) { gradient.push(value) });
-    console.log('$scope.colors', $scope.colors, $scope.colors.join(', '));
 
     $scope.gradient = {
       background: '-webkit-linear-gradient(left, ' + gradient.join(', ') + ') transparent',
