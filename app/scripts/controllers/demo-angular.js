2f0451fbc22933f775aa20b112a9be1d0249e97b
remy <remy_chaumard@silanis.com>
2014-01-24 13:41:06 -0500

    angular demo now filtering out empty values

--- app/scripts/controllers/demo-angular.js
+++ app/scripts/controllers/demo-angular.js
@@ -4,15 +4,17 @@ angular.module('portfolioApp')
 .controller('DemoAngularCtrl', [
   '$scope',
 function ($scope) {
-  $scope.initColors = function () { return ['#a2d39c', '#a2d39c', '#7bcdc8', 'rgb(110,207,246)', 'transparent'] }
+  $scope.initColors = function () { return ['', '#a2d39c', '#7bcdc8', 'rgb(110,207,246)', 'transparent'] }
 
-  $scope.updateGradient = function (colors) {
+  $scope.updateGradient = function () {
+    var colors = [];
+    angular.forEach($scope.colors, function (color) { if ( !!color ) colors.push(color) });
 
     var style = [];
-    style.push('background: linear-gradient(left, ' + $scope.colors.join(', ') + ') transparent;');
-    style.push('background: -webkit-linear-gradient(left, ' + $scope.colors.join(', ') + ') transparent;');
-    style.push('background: -moz-linear-gradient(left, ' + $scope.colors.join(', ') + ') transparent;');
-    style.push('background: -o-linear-gradient(left, ' + $scope.colors.join(', ') + ') transparent;');
+    style.push('background: linear-gradient(left, ' + colors.join(', ') + ') transparent;');
+    style.push('background: -webkit-linear-gradient(left, ' + colors.join(', ') + ') transparent;');
+    style.push('background: -moz-linear-gradient(left, ' + colors.join(', ') + ') transparent;');
+    style.push('background: -o-linear-gradient(left, ' + colors.join(', ') + ') transparent;');
     $scope.gradient = style.join('');
     $scope.gradientDisplay = style;
 
@@ -28,5 +30,5 @@ function ($scope) {
     $scope.handles.pop();
   };
 
-  $scope.$watch('colors', $scope.updateGradient, true);
+  $scope.$watch('colors | filter:"!!"', $scope.updateGradient, true);
 }]);
