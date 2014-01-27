da2ca48d5c48ae897c67fbe08705fbdee56ee44e
remy <remy_chaumard@silanis.com>
2014-01-26 23:15:26 -0500

    $.superHero now using options.speed and options.steps to compute animation logic

--- app/scripts/directives/demo-jquery-css.js
+++ app/scripts/directives/demo-jquery-css.js
@@ -6,13 +6,11 @@ angular.module('portfolioApp')
     restrict: 'A',
     templateUrl: 'views/demo-jquery-css.html',
     controller: function ($scope, $element) {
-
       $.widget('demo.superHero', {
         options: {
-          speed: 500,
+          speed: 400,
           steps: 3,
           target: undefined,
-          on: 'click',
         },
 
         _create: function () {
@@ -24,16 +22,16 @@ angular.module('portfolioApp')
         bindEvents: function (self) {
           for ( var event in self.options.trigger ) {
             $(self.options.trigger[event]).bind(event, function () {
-              setTimeout(function () { self.animate(self) }, 500);
+              setTimeout(function () { self.animate(self) }, self.options.speed);
             });
           }
         },
 
         animate: function (self) {
           self.$el.attr('hero', this.timer);
-          if ( self.timer === 3 ) return self.timer = 0;
+          if ( self.timer === self.options.steps ) return self.timer = 0;
           self.timer += 1;
-          setTimeout(function () { self.animate(self) }, 500);
+          setTimeout(function () { self.animate(self) }, self.options.speed);
         },
 
         _destroy: function () {
@@ -44,6 +42,18 @@ angular.module('portfolioApp')
         target: '#super-hero',
         trigger: { click: '.text-super-hero' },
       });
+
+      $('#switch-button').switchButton({
+        //width: 100,
+        //height: 40,
+        //button_width: 50,
+        labels_placement: "right",
+      }).bind('change', function (event) {
+        var isChecked = $(event.currentTarget).is(':checked');
+        var $target = $('#neon');
+        $target[ !isChecked ? 'removeClass' : 'addClass']('animate-lights-on');
+      });
+
     },
   }
 }]);
