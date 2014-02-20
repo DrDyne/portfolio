63e06861f7f5e7bbe7f0c62d4c6a4b353ec0850e
DrDyne <remy_chaumard@silanis.com>
2014-02-19 22:37:57 -0500

    removed mario-toad.png and build now also copies unminified images to dist/images

--- Gruntfile.js
+++ Gruntfile.js
@@ -273,6 +273,7 @@ module.exports = function (grunt) {
             'views/{,*/}*.html',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
+            'images/**.*',
             'fonts/*'
           ]
         }, {
